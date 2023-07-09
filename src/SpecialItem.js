bd977195bd275f040c5ccdaff70b77c1179e43c8
ariasmarmando <115955289+ariasmarmando@users.noreply.github.com>
2023-07-08 22:15:12 -0400

    Added Specials Components

---
+++ src/SpecialItem.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+const SpecialItem = (props) => {
+  return (
+    <div className="S-itemWrapper">
+      <img src={props.specialItem} alt="Special Menu Item" />
+      <p id="spItemName">{props.specialItemName}</p>
+      <p id="spItemPrice">{props.specialItemPrice}</p>
+      <p id="spItemDescription">{props.SpecialDesc}</p>
+      <a href={props.SpecialItemLink}>
+        Order for delivery{props.SpecialItemLinkIMG}
+      </a>
+    </div>
+  );
+};
+export default SpecialItem;
--- src/SpecialsWrapper.js
+++ src/SpecialsWrapper.js
@@ -1,11 +1,42 @@
 import "./App.css";
 import RegButton from "./RegButton";
+import SpecialItem from "./SpecialItem";
+import specialItem1 from "./Images/greek salad.jpg";
+
+const specialItemList1 = "Greek Salad";
+const specialItemPrice1 = 12.99;
+const specialItemDesc1 =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 const SpecialsWrapper = () => {
   return (
     <div className="nobgContainer">
       <p>This week's specials!</p>
       <RegButton buttonName="Online Menu" />
+      <SpecialItem
+        specialItem={specialItem1}
+        specialItemName={specialItemList1}
+        specialItemPrice={specialItemPrice1}
+        SpecialDesc={specialItemDesc1}
+        SpecialItemLink="./"
+        SpecialItemLinkIMG=""
+      />
+      <SpecialItem
+        specialItem={specialItem1}
+        specialItemName={specialItemList1}
+        specialItemPrice={specialItemPrice1}
+        SpecialDesc={specialItemDesc1}
+        SpecialItemLink="./"
+        SpecialItemLinkIMG=""
+      />
+      <SpecialItem
+        specialItem={specialItem1}
+        specialItemName={specialItemList1}
+        specialItemPrice={specialItemPrice1}
+        SpecialDesc={specialItemDesc1}
+        SpecialItemLink="./"
+        SpecialItemLinkIMG=""
+      />
     </div>
   );
 };
