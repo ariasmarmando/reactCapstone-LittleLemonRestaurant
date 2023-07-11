27720f2e1cc4f8c8202f722d72fa8573bf3c7f71
ariasmarmando <115955289+ariasmarmando@users.noreply.github.com>
2023-07-10 22:02:48 -0400

    testimonials completed

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,6 +1,46 @@
 import "../App.css";
+import RegButton from "./RegButton";
+import TestimonialCard from "./TestimonialCard";
+import { FaUserCircle } from "react-icons/fa";
 
 const Testimonials = () => {
-  return null;
+  return (
+    <div className="greenContainer">
+      <div className="Testimonials-container">
+        <h2 style={{ color: "white" }}>Testimonials</h2>
+        <div>
+          <TestimonialCard
+            userIMG={<FaUserCircle fontSize="1em" />}
+            reviewName="Jae"
+            reviewReview="Decent food"
+          />
+        </div>
+        <div>
+          <TestimonialCard
+            userIMG=""
+            reviewName="John"
+            reviewReview="Greate Place"
+          />
+        </div>
+        <div>
+          <TestimonialCard
+            userIMG=""
+            reviewName="Jin"
+            reviewReview="Love it here"
+          />
+        </div>
+        <div>
+          <TestimonialCard
+            userIMG=""
+            reviewName="Jake"
+            reviewReview="Had a decent time"
+          />
+        </div>
+        <div id="tes-button">
+          <RegButton buttonName="View More" />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Testimonials;
