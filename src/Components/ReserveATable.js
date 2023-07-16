4ad72d7c8d80ed9caefe0b667a46551b575968d4
ariasmarmando <115955289+ariasmarmando@users.noreply.github.com>
2023-07-15 23:20:30 -0400

    table component almost finished

--- src/Components/ReserveATable.js
+++ src/Components/ReserveATable.js
@@ -1,11 +1,12 @@
 import "../ReserveATable_CSS.css";
 import { useState } from "react";
+import foodPlate from "../Images/foodPlate.svg";
 
 const ReserveATable = () => {
   const [partySize, setPartySize] = useState(1);
 
   const addPerson = () => {
-    if (partySize < 6) {
+    if (partySize < 20) {
       setPartySize((count) => count + 1);
     }
   };
@@ -22,25 +23,270 @@ const ReserveATable = () => {
       <div className="Table-view">
         <div id={partySize < 3 ? "mini-circle" : ""}></div>
         <div id={partySize === 2 ? "mini-circle2" : ""}></div>
-        <div id={partySize > 2 ? "mini-circle3" : ""}></div>
-        <div id={partySize > 3 ? "mini-circle4" : ""}></div>
-        <div id={partySize > 2 ? "mini-circle5" : ""}></div>
-        <div id={partySize > 2 ? "mini-circle6" : ""}></div>
-        <div id={partySize > 2 ? "Squared-Table" : "Circle"}>
-          <div id={partySize < 3 ? "Plate-circle1" : ""}></div>
-          <div id={partySize === 2 ? "Plate-circle2" : ""}></div>
-          <div id={partySize > 2 ? "Plate-circle3" : ""}></div>
-          <div id={partySize > 3 ? "Plate-circle4" : ""}></div>
-          <div id={partySize > 2 ? "Plate-circle5" : ""}></div>
-          <div id={partySize > 2 ? "Plate-circle6" : ""}></div>
+        <div id={(partySize > 2) & (partySize < 5) ? "mini-circle3" : ""}></div>
+        <div id={(partySize > 3) & (partySize < 5) ? "mini-circle4" : ""}></div>
+        <div id={(partySize > 2) & (partySize < 5) ? "mini-circle5" : ""}></div>
+        <div id={(partySize > 2) & (partySize < 5) ? "mini-circle6" : ""}></div>
+        <div id={(partySize > 4) & (partySize < 7) ? "mini-circle7" : ""}></div>
+        <div id={(partySize > 4) & (partySize < 7) ? "mini-circle8" : ""}></div>
+        <div id={(partySize > 4) & (partySize < 7) ? "mini-circle9" : ""}></div>
+
+        <div
+          id={(partySize > 4) & (partySize < 7) ? "mini-circle10" : ""}
+        ></div>
+        <div
+          id={(partySize > 4) & (partySize < 7) ? "mini-circle11" : ""}
+        ></div>
+        <div id={partySize === 6 ? "mini-circle12" : ""}></div>
+        <div id={partySize > 6 ? "mini-circle13" : ""}></div>
+        <div id={partySize > 6 ? "mini-circle14" : ""}></div>
+        <div id={partySize > 6 ? "mini-circle15" : ""}></div>
+        <div id={partySize === 8 ? "mini-circle16" : ""}></div>
+        <div id={partySize > 6 ? "mini-circle17" : ""}></div>
+        <div id={partySize > 6 ? "mini-circle18" : ""}></div>
+        <div id={partySize > 6 ? "mini-circle19" : ""}></div>
+        <div id={partySize > 6 ? "mini-circle20" : ""}></div>
+        <div
+          id={
+            partySize < 3
+              ? "Circle"
+              : partySize < 5
+              ? "Squared-Table"
+              : partySize < 7
+              ? "Longer-Table"
+              : partySize > 6
+              ? "Big-Party"
+              : null
+          }
+        >
+          <div id={partySize < 3 ? "Plate-circle1" : ""}>
+            {partySize < 3 ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={partySize === 2 ? "Plate-circle2" : ""}>
+            {partySize === 2 ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 2) & (partySize < 7) ? "Plate-circle3" : ""}>
+            {" "}
+            {(partySize > 2) & (partySize < 7) ? (
+              <img
+                src={foodPlate}
+                alt="Plate of food"
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 3) & (partySize < 7) ? "Plate-circle4" : ""}>
+            {" "}
+            {(partySize > 3) & (partySize < 7) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 2) & (partySize < 7) ? "Plate-circle5" : ""}>
+            {" "}
+            {(partySize > 2) & (partySize < 7) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 2) & (partySize < 7) ? "Plate-circle6" : ""}>
+            {" "}
+            {(partySize > 2) & (partySize < 7) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 4) & (partySize < 7) ? "Plate-circle7" : ""}>
+            {" "}
+            {(partySize > 4) & (partySize < 7) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 5) & (partySize < 7) ? "Plate-circle8" : ""}>
+            {" "}
+            {(partySize > 5) & (partySize < 7) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 6) & (partySize < 9) ? "Plate-circle9" : ""}>
+            {" "}
+            {(partySize > 6) & (partySize < 9) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 6) & (partySize < 9) ? "Plate-circle10" : ""}>
+            {" "}
+            {(partySize > 6) & (partySize < 9) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 6) & (partySize < 9) ? "Plate-circle11" : ""}>
+            {" "}
+            {(partySize > 6) & (partySize < 9) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 6) & (partySize < 9) ? "Plate-circle12" : ""}>
+            {" "}
+            {(partySize > 6) & (partySize < 9) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 6) & (partySize < 9) ? "Plate-circle13" : ""}>
+            {" "}
+            {(partySize > 6) & (partySize < 9) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 6) & (partySize < 9) ? "Plate-circle14" : ""}>
+            {" "}
+            {(partySize > 6) & (partySize < 9) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 6) & (partySize < 9) ? "Plate-circle15" : ""}>
+            {" "}
+            {(partySize > 6) & (partySize < 9) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
+          <div id={(partySize > 7) & (partySize < 9) ? "Plate-circle16" : ""}>
+            {" "}
+            {(partySize > 7) & (partySize < 9) ? (
+              <img
+                alt="Plate of food"
+                src={foodPlate}
+                style={{
+                  width: "inherit",
+                  height: "auto",
+                }}
+              />
+            ) : null}
+          </div>
         </div>
       </div>
       <div id="Party-button">
         <h2>Party Size</h2>
         <h1>{partySize}</h1>
-        <button onClick={removePerson}>-</button>
-        <button onClick={addPerson}>+</button>
+        <button
+          id={partySize > 1 ? "removePerson" : "removePerson_OFF"}
+          onClick={partySize > 1 ? removePerson : null}
+        >
+          -
+        </button>
+        <button
+          id={partySize < 8 ? "addPerson" : "addPerson_OFF"}
+          onClick={partySize < 8 ? addPerson : null}
+        >
+          +
+        </button>
       </div>
+      {partySize > 7 ? (
+        <h2 id="PartyLimit">For parties over 8 people, please call us.</h2>
+      ) : null}
+
       <div id="Date">
         <h3>Select a Date:</h3>
       </div>
